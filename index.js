c1487ea6b591dd1a821cefcb5eb7e79d2abf8dd6
shani <shhhaniburde@gmail.com>
2021-12-18 19:41:26 -0500

    commit tree 3829a6d623a62e3f230820446bf2253ee691a12d

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+// function addingEventListener() {
+//     const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
+// }
+
+// addingEventListener();
 function addingEventListener() {
     const input = document.getElementById('input');
 
-function clickAlert() {
-  alert('I was clicked!');
+    function clickAlert() {
+      alert('I was clicked!')
+    }
+
+  input.addEventListener('click', clickAlert)
 }
 
-input.addEventListener('click', clickAlert);
-}
 
-addingEventListener();
+
+
